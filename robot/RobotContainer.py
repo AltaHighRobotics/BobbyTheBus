74aca9036e5fc5009e20a12271ded5e0cc64eab3
Ethanscharlie <ethanscharlie@protonmail.com>
2025-02-12 16:43:45 -0700

    Button should make something happen

--- robot/RobotContainer.py
+++ robot/RobotContainer.py
@@ -1,10 +1,15 @@
 from constants import DRIVER_CONTROLLER_PORT
 
 from subsystems.BasicSparkMaxSubsystem import BasicSparkMaxSubsystem
+from commands.TestSparkMaxCommand import TestSparkMaxCommand
 from wpilib import XboxController
+from commands2.button import JoystickButton
 
 class RobotContainer:
     def __init__(self) -> None:
         self.driverController = XboxController(DRIVER_CONTROLLER_PORT)
 
         self.spark = BasicSparkMaxSubsystem()
+
+        JoystickButton(self.driverController, 5).whileTrue(TestSparkMaxCommand(self.spark))
+
--- robot/commands/TestSparkMaxCommand.py
+++ robot/commands/TestSparkMaxCommand.py
@@ -1,5 +1,10 @@
-from commands2 import Command
+from commands2.command import Command
+from subsystems.BasicSparkMaxSubsystem import BasicSparkMaxSubsystem
 
 class TestSparkMaxCommand(Command):
-    def __init__(self) -> None:
+    def __init__(self, sparkmaxSubsystem: BasicSparkMaxSubsystem) -> None:
         super().__init__()
+        self.turn = sparkmaxSubsystem
+
+    def execute(self) -> None:
+        self.turn.testTurn()
